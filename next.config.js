205a48918624152281693105a510e4f4ab6a6d97
Saumyapratim Das <saumyadas2017@gmail.com>
2023-08-20 01:37:01 +0530

    SVG render issue fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
     images: {
         domains: ["misc.scdn.co", "i.scdn.co", "geo-media.beatsource.com", "i1.sndcdn.com", "media.pitchfork.com", "seed-mix-image.spotifycdn.com", "vudxxkevlbcmixjyojoh.supabase.co"]
-      }
+      },
+    swcMinify: false
 }
 
 module.exports = nextConfig
