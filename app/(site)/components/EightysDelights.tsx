248cbd86f116d83a47224974c8ecee18dd3140aa
Saumyapratim Das <saumyadas2017@gmail.com>
2023-08-20 11:31:19 +0530

    Shuffle

--- app/(site)/components/EightysDelights.tsx
+++ app/(site)/components/EightysDelights.tsx
@@ -8,14 +8,24 @@ interface EightysDelightsProps {
   songs: Song[];
 }
 
+const shuffleArray = (array: any[]) => {
+  const shuffled = [...array];
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled;
+};
+
 const EightysDelights: React.FC<EightysDelightsProps> = ({ songs }) => {
   const onPlay = useOnPlay(songs);
 
   const filteredSongs = songs.filter(
     (song) => song.year >= 1980 && song.year <= 1990
   );
+  const shuffledSongs = shuffleArray(filteredSongs).slice(0, 5); // Take 5 random songs
 
-  if (filteredSongs.length === 0) {
+  if (shuffledSongs.length === 0) {
     return (
       <div className="mt-4 text-neutral-400">No songs available.</div>
     );
@@ -35,7 +45,7 @@ const EightysDelights: React.FC<EightysDelightsProps> = ({ songs }) => {
         mt-4
       "
     >
-      {filteredSongs.map((item) => (
+      {shuffledSongs.map((item) => (
         <SongItem
           onClick={(id: string) => onPlay(id)}
           key={item.id}
